ee9f1f738071f57d8728585cedf2ca2169d69add
Itunuoluwa1 <okikiolami2005@gmail.com>
2022-11-04 23:43:26 +0100

    first commit

---
+++ signup.js
@@ -0,0 +1,7 @@
+submitEl = document.getElementById("submit-el");
+console.log(submitEl);
+
+function submit() {
+  console.log("This form has been submitted");
+  submitEl.textcontent = "THIS FORM HAS BEEN SUBMITTED";
+}
